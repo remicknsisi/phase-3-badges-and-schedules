552cf0aac23baec8a6607ba76c973cffd1249400
Sienna <remicknsisi@gmail.com>
2023-03-09 12:09:06 -0500

    commit tree e51c704b8fbecdabb3c528bb51c78a5291f9d11c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+    arr.map do |n|
+        "Hello, my name is #{n}."
+    end
+end
+
+def assign_rooms arr
+    arr.each_with_index { |name, index| 
+        arr[name] = index
+        "Hello, #{name}! You'll be assigned to room #{index}!"
+    }
+end
